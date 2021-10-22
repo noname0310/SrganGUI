460f12f62e8818a6e63f31d7f516dec76e65cb57
noname <48761044+noname0310@users.noreply.github.com>
2021-10-22 12:13:27 +0900

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace SrganGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox
+            // 
+            this.comboBox.DisplayMember = "23232";
+            this.comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "realesrgan-x4plus",
+            "realesrnet-x4plus",
+            "esrgan-x4"});
+            this.comboBox.Location = new System.Drawing.Point(12, 12);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(134, 23);
+            this.comboBox.TabIndex = 0;
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox.Location = new System.Drawing.Point(0, 41);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.Size = new System.Drawing.Size(534, 280);
+            this.textBox.TabIndex = 2;
+            this.textBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.TextBox_DragDrop);
+            this.textBox.DragOver += new System.Windows.Forms.DragEventHandler(this.TextBox_DragOver);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 321);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.comboBox);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "REALESRGAN GUI";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace SrganGUI
+{
+    public partial class Form1 : Form
+    {
+        private static readonly string realesrganCmd = "-i \"{0}\" -o \"{1}\"";
+        private static readonly string realesrnetCmd = "-i \"{0}\" -o \"{1}\" -n realesrnet-x4plus";
+        private static readonly string esrganCmd = "-i \"{0}\" -o \"{1}\" -n esrgan-x4";
+        private readonly List<Process> processList = new();
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox.SelectedIndex = 0;
+            textBox.AllowDrop = true;
+        }
+
+        private void TextBox_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data?.GetDataPresent(DataFormats.FileDrop) ?? false)
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void TextBox_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data?.GetData(DataFormats.FileDrop) is not string[] filePathArray)
+                return;
+
+            string command;
+            string appendText;
+            switch (comboBox.SelectedIndex)
+            {
+                case 0:
+                    command = realesrganCmd;
+                    appendText = "-realesrgan";
+                    break;
+                case 1:
+                    command = realesrnetCmd;
+                    appendText = "-realesrgnet";
+                    break;
+                case 2:
+                    command = esrganCmd;
+                    appendText = "-esrgan";
+                    break;
+                default:
+                    goto case 0;
+            }
+
+            foreach (var item in processList)
+            {
+                try
+                {
+                    item.Kill();
+                    item.Dispose();
+                }
+                catch (InvalidOperationException) { }
+            }
+            processList.Clear();
+            textBox.Clear();
+            foreach (var path in filePathArray)
+            {
+                var dir = Path.GetDirectoryName(path) ?? throw new Exception();
+                var fileName = Path.GetFileNameWithoutExtension(path);
+                var fileExt = Path.GetExtension(path);
+                var newPath = Path.Combine(dir, $"{fileName}{appendText}{fileExt}");
+                var process = new Process()
+                {
+                    StartInfo = new()
+                    {
+                        FileName = "realesrgan-ncnn-vulkan.exe",
+                        Arguments = string.Format(command, path, newPath),
+                        UseShellExecute = false,
+                        CreateNoWindow = true,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                    },
+                    SynchronizingObject = this,
+                    EnableRaisingEvents = true,
+                };
+                process.Exited += (o, _) => {
+                    var p = (Process?)o;
+                    textBox.AppendText($"[{p?.Id}]task exited: {p?.ExitCode} \r\n");
+                    process?.Dispose();
+                };
+                process.OutputDataReceived += (o, e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data))
+                        textBox.AppendText($"[{((Process)o).Id}]{e.Data}\r\n");
+                };
+                process.ErrorDataReceived += (o, e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data))
+                        textBox.AppendText($"[{((Process)o).Id}]{e.Data}\r\n");
+                };
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+                processList.Add(process);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            foreach (var item in processList)
+            {
+                try
+                {
+                    item.StandardInput.Close();
+                    item.Kill();
+                    item.Dispose();
+                }
+                catch (InvalidOperationException) { }
+            }
+        }
+    }
+}
